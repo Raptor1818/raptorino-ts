14f5635e32913792437922eba7d83dec15e2e956
francyoppureno@gmail.com <francyoppureno@gmail.com>
2024-02-05 22:11:21 +0100

    Added star background
    from https://www.youtube.com/watch?v=0siQoaXMmzk

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import css from './App.module.css'
 import Navbar from './components/Navbar';
 //import Footer from './components/Footer';
 import Body from './components/Body';
+import StarsCanvas from './components/StarBackground';
 
 function App() {
   return (
     <>
       <main className={css.mainContainer}>
+        <StarsCanvas></StarsCanvas>
         <Navbar></Navbar>
         <Body></Body>
         {/*<Footer></Footer>*/}
