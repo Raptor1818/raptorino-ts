38da8c269ec1d1a5b423c03a2ec63002ff1a291b
Raptor <francyoppureno@gmail.com>
2024-02-07 11:54:25 +0100

    Added border to section links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,12 +9,12 @@ function Navbar({}: Props) {
   return (
     <nav className={css.navbar}>
       <h1 className={css.logoTitle}>Raptorino</h1>
-      <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">About</a></li>
-        <li><a href="">Projects</a></li>
-        <li><a href="">Links</a></li>
-      </ul>
+      <div className={css.navSectionLinks}>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Projects</a>
+        <a href="">Links</a>
+      </div>
       <ul className={css.navSocialIcons}>
         <li><a href="https://twitter.com/_Raptorr" target="_blank" rel="noopener noreferrer"></a><FaXTwitter  /></li>
         <li><a href="https://github.com/Raptor1818" target="_blank" rel="noopener noreferrer"></a><FaGithub /></li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
       colors:{
         'default-bg': "#000000",
         'default-text': "rgb(229,229,229)",
-        'accent-text': "#FF3091",
+        'accent-text': "#6666EE",
         'dark-text': "rgb(170,170,170)",
         'default-border': '#33353F',//'rgb(50,50,50)',
         'accent-from': '#6666EE',
