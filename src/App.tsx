05e80dd7463ed105eb6311296314ed4e9d7cb694
francyoppureno@gmail.com <francyoppureno@gmail.com>
2024-02-12 19:04:39 +0100

    component usage adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import css from './App.module.css'
-import Navbar from './components/Navbar';
-//import Footer from './components/Footer';
 import Body from './components/Body';
 import StarsCanvas from './components/StarBackground';
 
@@ -9,9 +7,7 @@ function App() {
     <>
       <main className={css.mainContainer}>
         <StarsCanvas></StarsCanvas>
-        <Navbar></Navbar>
         <Body></Body>
-        {/*<Footer></Footer>*/}
       </main>
     </>
   )
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -2,15 +2,25 @@ import css from '@/styles/body.module.css';
 
 import Home from '@/components/Home';
 import About from '@/components/About';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 type Props = {}
 
 function Body({}: Props) {
   return (
-    <div className={css.bodyContainer}>
-      <Home></Home>
-      <About></About>
-    </div>
+    <>
+
+      <Navbar />
+
+      <div className={css.bodyContainer}>
+        <Home />
+        <About />
+      </div>
+
+      <Footer />
+
+    </>
   )
 }
 
