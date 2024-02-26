5bff07e753376e2a1a47a7ac4727e7bc32dda5ff
Raptor <francyoppureno@gmail.com>
2024-02-26 09:55:29 +0100

    added langs to cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+// Import ReactElement from 'react'
+import React, { ReactElement } from 'react'; 
 import css from '@/styles/projects.module.css';
 import Header from '@/components/ui/Header';
 import { IoConstruct } from "react-icons/io5";
 import ProjectCard from '@/components/ui/ProjectCard';
+import {
+  SiReact,
+  SiTypescript,
+  SiTailwindcss
+} from 'react-icons/si';
 
 type Project = {
   id: number;
   title: string;
   desc: string;
+  langs: ReactElement[]; // Use ReactElement[]
 };
 
 const projectList: Project[] = [
@@ -15,28 +22,22 @@ const projectList: Project[] = [
     id: 1,
     title: 'Raptorino',
     desc: 'My personal website',
+    langs: [<SiReact />, <SiTypescript />, <SiTailwindcss />] // Directly use JSX elements
   },
-  {
-    id: 2,
-    title: 'Another Project',
-    desc: 'Description of another project',
-  },
+  // Additional projects...
 ];
 
-type Props = {}
-
-const Projects = (props: Props) => {
+const Projects = () => {
   return (
     <section id='projects'>
-      <Header icon={IoConstruct}>
-        My projects
-      </Header>
+      <Header icon={IoConstruct}>My projects</Header>
       <div className={css.projectsContainer}>
         {projectList.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             desc={project.desc}
+            langs={project.langs}
           />
         ))}
       </div>
--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-
-import css from '@/styles/ui/projectCard.module.css'
+import React, { ReactElement } from 'react'; // Import ReactElement
+import css from '@/styles/ui/projectCard.module.css';
 
 type Props = {
   title: string,
-  desc: string
+  desc: string,
+  langs: ReactElement[]; // Change to ReactElement[]
 }
 
 const ProjectCard = (props: Props) => {
@@ -15,8 +15,13 @@ const ProjectCard = (props: Props) => {
         {props.title}</h3>
       <p className={css.cardDesc}>
         {props.desc}</p>
+      <div className={css.langSection}>
+        {props.langs.map((icon, index) => (
+          React.cloneElement(icon, { key: index }) // Use React.cloneElement to assign key
+        ))}
+      </div>
     </div>
   )
 }
 
-export default ProjectCard
+export default ProjectCard;
