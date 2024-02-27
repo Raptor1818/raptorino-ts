be59820582ff8315b8f758fc829ebcc70a77ccd8
Raptor <francyoppureno@gmail.com>
2024-02-27 08:58:12 +0100

    Update ProjectCard.tsx

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,15 +1,15 @@
-import React, { ReactElement } from 'react'; // Import ReactElement
+import React, { ReactElement } from 'react';
 import css from '@/styles/ui/projectCard.module.css';
 
 type Props = {
   title: string,
   desc: string,
-  langs: ReactElement[]; // Change to ReactElement[]
+  langs: ReactElement[];
 }
 
 const ProjectCard = (props: Props) => {
   return (
-    <div>
+    <div >
       <img src="" alt="" />
       <h3 className={css.cardTitle}>
         {props.title}</h3>
@@ -17,7 +17,7 @@ const ProjectCard = (props: Props) => {
         {props.desc}</p>
       <div className={css.langSection}>
         {props.langs.map((icon, index) => (
-          React.cloneElement(icon, { key: index }) // Use React.cloneElement to assign key
+          React.cloneElement(icon, { key: index })
         ))}
       </div>
     </div>
